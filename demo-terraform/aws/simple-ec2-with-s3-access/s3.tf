1b19d3da356da51fa5aeaa5b33fd682054614780
Tenable.cs <admin@accurics.com>
2023-07-18 12:56:22 -0400

    Auto Remediation By Tenable.cs

--- demo-terraform/aws/simple-ec2-with-s3-access/s3.tf
+++ demo-terraform/aws/simple-ec2-with-s3-access/s3.tf
@@ -1,28 +1,33 @@
 
 resource "random_string" "bucket_suffix" {
-    upper = false
-    special = false
-    length = 32
+  upper   = false
+  special = false
+  length  = 32
 }
 
 resource "aws_s3_bucket" "bucket" {
-    bucket = "${replace(local.user_mail,"@","-")}-${random_string.bucket_suffix.result}"
+  bucket = "${replace(local.user_mail, "@", "-")}-${random_string.bucket_suffix.result}"
 
-    tags = local.default_tags
+  tags = local.default_tags
+
+  versioning {
+    enabled    = true
+    mfa_delete = true
+  }
 }
 
 resource "aws_s3_bucket_acl" "bucket" {
-    bucket = aws_s3_bucket.bucket.id
-    acl    = "private"
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
-    bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.bucket.id
 
-    block_public_acls   = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
